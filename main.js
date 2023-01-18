2eccf19ba9c8628ed6acbfe1c81b54d72ce15e2d
Amber Oglesby <aoglesby5795@gmail.com>
2023-01-18 00:14:16 -0500

    add content

---
+++ main.js
@@ -0,0 +1,7 @@
+var img = document.getElementById("clickme");
+img.addEventListener("click", function(){
+  if(img.src != "https://raw.githubusercontent.com/aogles/FB/main/4d21fe6a92af4ae5cc23668bc02559c9.jpeg"){
+    img.src = "https://raw.githubusercontent.com/aogles/FB/main/4d21fe6a92af4ae5cc23668bc02559c9.jpeg";}
+  else{
+    img.src = "https://raw.githubusercontent.com/aogles/FB/main/1648053397141461853.webp";}});
+
